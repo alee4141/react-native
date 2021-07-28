f64e211e3d51c03bdb1f3d8a860394cc21d842dd
Alee Swedat <aswedat70@icloud.com>
2021-07-28 11:50:30 -0500

    activity indicator

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, View, Text } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -37,6 +38,20 @@ class Directory extends Component {
                 />
             );
         };
+
+        if (this.props.campsites.isLoading) {
+            return <Loading />
+        }
+
+        if (this.props.campsites.errMess) {
+            return (
+                <View>
+                    <Text>
+                        {this.props.campsites.errMess}
+                    </Text>
+                </View>
+            )
+        }
 
         return (
             <FlatList
--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { View, Text, ScrollView  } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card } from 'react-native-elements'; 
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -13,6 +14,19 @@ const mapStateToProps = state => {
 }
 
 function RenderItem({item}) {
+    const {item} = props;
+
+    if(props.isLoading) {
+        return <Loading />
+    }
+    if(props.errMess) {
+        return (
+            <View>
+                <Text>{props.errMess}</Text>
+            </View>
+        )
+    }
+
     if (item) {
         return (
             <Card featuredTitle={item.name} image={{uri: baseUrl + item.image}}>
@@ -34,9 +48,9 @@ class Home extends Component {
     render() {
         return(
             <ScrollView>
-                <RenderItem item={this.state.campsites.campsites.filter(campsite => campsite.featured)[0]} />
-                <RenderItem item={this.state.promotions.promotions.filter(promotion => promotion.featured)[0]} />
-                <RenderItem item={this.state.partners.partners.filter(partner => partner.featured)[0]} />
+                <RenderItem item={this.state.campsites.campsites.filter(campsite => campsite.featured)[0]} isLoading={this.props.campsites.isLoading} errMess={this.props.campsites.errMess} />
+                <RenderItem item={this.state.promotions.promotions.filter(promotion => promotion.featured)[0]} isLoading={this.props.promotions.isLoading} errMess={this.props.promotions.errMess} />
+                <RenderItem item={this.state.partners.partners.filter(partner => partner.featured)[0]} isLoading={this.props.partners.isLoading} errMess={this.props.partners.errMess} />
             </ScrollView>
         )
     }
